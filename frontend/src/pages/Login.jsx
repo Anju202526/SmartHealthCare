9b34b261b6637b491c84fc666022e2b66ae7d252
Anju202526 <anjuaravi1995@gmail.com>
2026-03-27 22:57:04 +0000

    Fix formatting issues in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -104,7 +104,7 @@ const Login = () => {
     setPassword(creds[demoRole].password);
     setError('');
   };
-
+}
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100
       relative overflow-hidden px-4">
