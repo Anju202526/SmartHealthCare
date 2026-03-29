4e81fc48e57e001a04b15895c9db8f8f245313d2
Anju202526 <anjuaravi1995@gmail.com>
2026-03-29 21:36:13 +0100

    Remove default frontend URL from CORS configuration

--- server.js
+++ server.js
@@ -22,9 +22,8 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
-    credentials: true,
-    optionsSuccessStatus: 200,
+    origin: process.env.FRONTEND_URL
+    credentials: true
   })
 );
 
