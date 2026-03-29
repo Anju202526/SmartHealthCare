4a51dcae79aa9b049abf7e373f443ede172ba81e
Anju202526 <anjuaravi1995@gmail.com>
2026-03-29 10:58:13 +0100

    Delete frontend directory

--- frontend/src/hooks/useAuth.js
+++
@@ -1,4 +0,0 @@
-// Convenience re-export so components can import from either location:
-//   import { useAuth } from '../hooks/useAuth'
-//   import { useAuth } from '../context/AuthContext'
-export { useAuth } from '../context/AuthContext';
